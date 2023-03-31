9e288ce3fb6263d7eea42bf35393283aed94f1ba
Gustavo Ferreira <gfmb94@gmail.com>
2023-03-31 11:18:39 -0300

    Aula 17 - Vetores

---
+++ Aulas/Aula17.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aula17 : MonoBehaviour
+{
+    // Aula 17 - Vetores
+
+    void Start()
+    {
+        int [] array = new int[10];
+
+        for(int i = 0; i <=0; i++)
+        {
+            array [i] = 1 + i;
+
+            print(array[i]);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
